45369cfcb2acbe8cdb5c2602fcaa88b692b47a8a
Aimee MacDonald <aimeelmacdonald@gmail.com>
2018-11-04 10:53:37 +0200

    reels and tweeks

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,13 @@
+var el_hnmsmall1 = document.getElementById("hnmsmall1");
+var el_hnmsmall2 = document.getElementById("hnmsmall2");
+var el_hnmbig = document.getElementById("hnmbig");
+var el_imgsfx = document.getElementById("imgsfx");
+
+el_hnmsmall1.style.backgroundImage = "url(../images/hnmsmall1.jpg)";
+el_hnmsmall2.style.backgroundImage = "url(../images/hnmsmall2.jpg)";
+el_hnmbig.style.backgroundImage = "url(../images/hnmbig.jpg)";
+el_imgsfx.style.backgroundImage = "url(../images/sfx.jpg)";
+
 if(window.innerWidth >= 800){
   var el_artboxes = document.getElementsByClassName("art-box");
 
